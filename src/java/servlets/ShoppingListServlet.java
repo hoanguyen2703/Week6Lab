d50aff6d2d4d5d802c83d89b506403efe9c72138
khanhhoanguyen <khanhhoanguyen@10.211.55.3>
2023-02-24 21:09:30 -0700

    Modified, Tested and Completed Application Modified ShoppingListServlet.java, register.jsp, shoppingList.jsp Tested and completed the application

--- src/java/servlets/ShoppingListServlet.java
+++ src/java/servlets/ShoppingListServlet.java
@@ -6,11 +6,12 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -18,69 +19,89 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ShoppingListServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ShoppingListServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ShoppingListServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+  
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        HttpSession session = request.getSession(false);
+        String username = null;
+        
+        if (session != null){
+            username = (String)session.getAttribute("username");
+        }
+        
+        if(username != null){
+            String action = request.getParameter("action");
+            
+            if(action.equals("logout")){ 
+                session.invalidate();
+            }
+            else {
+                request.setAttribute("username", username);
+                getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp" )
+                    .forward(request, response); 
+                return;
+            }
+        }
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/register.jsp" )
+            .forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        //gets value of hidden input tag named action
+        String action = request.getParameter("action");
+        
+        //only instantiates if object exists otherwise null
+        HttpSession session = request.getSession(false);
+        
+        switch (action){
+            case "register":
+                //creation of Session, String, and ArrayList object
+                session = request.getSession();
+                String username = request.getParameter("username");
+                ArrayList<String> items = new ArrayList<>();
+                
+                //storing String and ArrayList object within session
+                session.setAttribute("username", username);
+                session.setAttribute("items", items);
+                break;
+                
+            case "add":
+                //gets value of input tag with name item
+                String item = request.getParameter("item");
+                //gets ArrayList stored within session
+                items = (ArrayList<String>)session.getAttribute("items"); 
+                //add item to array and setting it once again in the session object
+                items.add(item);
+                session.setAttribute("items", items);
+                break;  
+                
+            case "delete":
+                items = (ArrayList<String>)session.getAttribute("items");
+                
+                for (int i = 0; i < items.size(); i++) {
+                    String elem = items.get(i);
+                    
+                    //if radio button is checked, value of tag is returned otherwise null
+                    String checked = request.getParameter(elem);
+                    
+                    if(elem.equals(checked)) {
+                        items.remove(i--);// decrement i to compensate
+                    }
+                }
+                session.setAttribute("items", items);
+                break;   
+        }
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp" )
+                    .forward(request, response); 
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    
 }
--- web/WEB-INF/shoppingList.jsp
+++ web/WEB-INF/shoppingList.jsp
@@ -4,14 +4,44 @@
     Author     : khanhhoanguyen
 --%>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Shopping List - Home</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Shopping List</h1>
+        <p>
+            Hello, ${username} 
+            <a href="ShoppingList?action=logout">Logout</a>
+        </p>
+        
+        <form action="" method="POST">
+            <h2>Add Items</h2>
+            <input type="text" name="item">
+            <input type="submit" value="Add Item">
+            <input type="hidden" name="action" value="add">
+        </form>
+        
+        <form action="" method="POST">
+            <ul>
+                <c:forEach var="item" items="${items}">
+                    <li> 
+                        <input type="radio" name="${item}" value="${item}">${item}
+                    </li>
+                </c:forEach>
+            </ul>
+            <input type="submit" value="Delete">
+            <input type="hidden" name="action" value="delete">
+        </form>
+        
+        <style>
+            li {             
+            list-style-type: none;
+            }
+        </style>
     </body>
 </html>
